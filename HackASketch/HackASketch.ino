42c70826d6f649d09c51402f8d53df48a39ebad0
Eric Chubaty <echubaty@gmail.com>
2016-12-03 13:21:19 -0600

    Finished Arduino file, supports 2 pots and an accelerometer

--- HackASketch/HackASketch.ino
+++ HackASketch/HackASketch.ino
@@ -1,44 +1,47 @@
-/*
-  ReadAnalogVoltage
-  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+const int accelX = A0;
+const int accelY = A1;
+const int accelZ = A2;
+const int lPot = A8;
+const int rPot = A9;
 
-  This example code is in the public domain.
-*/
+const float VOLT_REF = 3.3 / 5.0;
+const float zero_G = 512.0;
+const float scale = 102.3;
 
-// the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(57600);
-   // analogReference(EXTERNAL);
+  Serial.begin(9600);
+  
+  pinMode(accelX, INPUT);
+  pinMode(accelY, INPUT);
+  pinMode(accelZ, INPUT);
 
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
+  pinMode(lPot, INPUT);
+  pinMode(rPot, INPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  delay(250);
-  // read the input on analog pin 0:
-  int x = analogRead(A0);
-   delay(1); 
-    int y = analogRead(A1);
-       delay(1);
-    int z = analogRead(A1);
-       delay(1);
-  float zero_G = 512.0; //ADC is 0~1023 the zero g output equal to Vs/2
-  //ADXL335 power supply by Vs 3.3V
-  float scale = 102.3; //ADXL335330 Sensitivity is 330mv/g
+  delay(25);
 
- Serial.print(x);
- // Serial.print(((float)x - zero_G)/scale);
+  int pot0 = analogRead(lPot);
+  int pot1 = analogRead(rPot);
+
+  // read the input on analog pin 0:
+  int x = analogRead(accelX);
+  delay(5);
+  int y = analogRead(accelY);
+  delay(5);
+  int z = analogRead(accelZ);
+  delay(5);
+
+  Serial.print(pot0);
   Serial.print("\t");
- // Serial.print(((float)y - zero_G)/scale);
-    Serial.print(y);
-    Serial.print("\t");
-  Serial.print(((float)z - zero_G)/scale);
-  Serial.print("\n");
+  Serial.print(pot1);
+  Serial.print("\t");
+  Serial.print((((float)(x / VOLT_REF)) - zero_G)/scale);
+  Serial.print("\t");
+  Serial.print((((float)(y / VOLT_REF)) - zero_G)/scale);
+  Serial.print("\t");
+  Serial.println((((float)(z / VOLT_REF)) - zero_G)/scale );
 }
 
